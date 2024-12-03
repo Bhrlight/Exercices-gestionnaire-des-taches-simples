dc3e86f518286eda736d79b2e926c3b422613dcf
Bhrlight <lumenabonheur@gmail.com>
2024-12-03 21:13:16 +0100

    resolu 1

---
+++ exercices.js
@@ -0,0 +1,20 @@
+// afficher un message de bienvenue
+
+/console.log("Hello World");
+
+// Créer une variable de nombre entier et l'afficher
+
+let nbTache = 5;
+return nbTache;
+
+// Condition : Vérifier si le nombre de tâches est zéro
+
+if(nbTache === 0){
+    console.log('Aucune tâche à faire.')
+}
+
+// Ajouter une tâche à une liste (Tableau)
+
+const taches = [];
+taches.push("Faire les courses");
+return taches;
